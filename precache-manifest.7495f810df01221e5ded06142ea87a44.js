05d73732867090399e06a397d1bf6f7d14567040
Guilherme Guerreiro <guiguerreiro10@hotmail.com>
2020-10-27 20:12:42 +0000

    Updates

---
+++ precache-manifest.7495f810df01221e5ded06142ea87a44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3456906c63ff2ad121e1487186850b9d",
+    "url": "/react-gallery/index.html"
+  },
+  {
+    "revision": "f53bead67db3940483fe",
+    "url": "/react-gallery/static/css/main.4e686419.chunk.css"
+  },
+  {
+    "revision": "844bba125b38616c943d",
+    "url": "/react-gallery/static/js/2.ecec9d70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gallery/static/js/2.ecec9d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53bead67db3940483fe",
+    "url": "/react-gallery/static/js/main.3a66b4f5.chunk.js"
+  },
+  {
+    "revision": "9b1cbeb86537553bc853",
+    "url": "/react-gallery/static/js/runtime-main.49d20663.js"
+  }
+]);
